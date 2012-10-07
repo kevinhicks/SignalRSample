af0f6122030f0492b22197e0e031753fe92b2394
Kevin Hicks <keyholesoftware@gmail.com>
2012-10-07 07:16:06 -0400

    Created a nice calender, and a chat area using SignalR

---
+++ SignalRSample/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SignalR.Hubs;
+
+namespace SignalRSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+
+
+    [HubName("chatHub")]
+    public class ChatHub : Hub
+    {
+        public void Send(string msg, string sessnID)
+        {
+            Clients.addMessage(msg, sessnID);
+        }
+    }
+}
